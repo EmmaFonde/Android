9e0f76b4bef1af35891e2060a06a764210a2fae4
EmmaFonde <minigym65@hotmail.fr>
2019-01-30 11:58:29 +0100

    Commit

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myfirstapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +19,20 @@ public class MainActivity extends AppCompatActivity {
         Toast myToast = Toast.makeText(this, "Hello Toast!",
                 Toast.LENGTH_SHORT);
         myToast.show();
+    }
+    public void countMe (View view) {
+        // Get the text view.
+        TextView showCountTextView = (TextView)
+                findViewById(R.id.textView);
+
+        // Get the value of the text view.
+        String countString = showCountTextView.getText().toString();
+
+        // Convert value to a number and increment it.
+        Integer count = Integer.parseInt(countString);
+        count++;
+
+        // Display the new value in the text view.
+        showCountTextView.setText(count.toString());
     }
 }
